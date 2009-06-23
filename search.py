1818b5c87105c7970e12d12277f83fccd8669e1a
qingant <qingant@5564ac98-270e-11de-9ca7-1beefeb68fa5>
2009-06-23 20:40:27 +0000

    稍做美化，附送一个获取baidu搜索suggest结果用google搜索的小玩意

---
+++ search.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+# Filename:
+# Author:    E-mail:qingant@gmail.com
+# Lisence: GPL-2.0 
+
+import pythondo
+pythondo.SearchBar().mainloop()
+
+
+
+
+
+
+
+
+    
